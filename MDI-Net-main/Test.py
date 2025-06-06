45b78ee5fb77d72bfce7053811e0fe7d3f309727
shen123shen <1625360397@qq.com>
2025-06-06 21:46:45 +0800

    Update Test.py

--- MDI-Net-main/Test.py
+++ MDI-Net-main/Test.py
@@ -17,7 +17,7 @@ from tqdm import tqdm
 from sklearn.metrics import confusion_matrix
 import torch.nn as nn
 
-# 设置随机种子以确保结果的可重复性
+
 def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
@@ -37,7 +37,6 @@ def structure_loss(pred, mask):
 
 def test(test_loader, model, optimizer, epoch):
     model.eval()
-    # 创建一个空列表，用于存储每个批次的损失值。
     loss_list = []
     loss_P2_record = AvgMeter()
     gts_list = []
@@ -59,33 +58,25 @@ def test(test_loader, model, optimizer, epoch):
             loss_P2_record.update(loss_P1.data, 1)
             loss_list.append(loss_P2_record.show())
             # ----------添加P1,gts---------
-            gts_list.append(gts.squeeze(1).cpu().detach().numpy())  # 将真实标签添加到 gts 列表中。
-            if type(P1) is tuple:  # 检查模型输出是否为元组，如果是，则取第一个元素作为输出。
+            gts_list.append(gts.squeeze(1).cpu().detach().numpy())  
+            if type(P1) is tuple: 
                 P1 = P1[0]
-            P1 = P1.squeeze(1).cpu().detach().numpy()  # 将输出转换为NumPy数组。
-            P1_list.append(P1)  # 将预测结果添加到 preds 列表中。
+            P1 = P1.squeeze(1).cpu().detach().numpy() 
+            P1_list.append(P1)  
 
 
     mean_loss = np.mean([l.cpu().numpy() for l in loss_list])
     preds = np.array(P1_list).reshape(-1)
     gts = np.array(gts_list).reshape(-1)
-    # 使用 np.where 函数将预测结果 preds 和真实标签 gts 二值化。预测结果大于或等于阈值 config.threshold 时被视为1（正类）否则被视为0（负类）
     y_pre = np.where(preds >= 0.5, 1, 0)
-    # 同样，真实标签大于或等于0.5时被视为1，否则被视为0。
     y_true = np.where(gts >= 0.5, 1, 0)
-    # 使用 confusion_matrix 函数计算二值化后的预测结果和真实标签之间的混淆矩阵。
     confusion = confusion_matrix(y_true, y_pre)
 
     TN, FP, FN, TP = confusion[0, 0], confusion[0, 1], confusion[1, 0], confusion[1, 1]
-    # 正确预测的样本数占总样本数的比例。
     accuracy = float(TN + TP) / float(np.sum(confusion)) if float(np.sum(confusion)) != 0 else 0
-    # sensitivity（敏感性/召回率）：正确预测为正类的样本数占所有实际正类样本数的比例。
     sensitivity = float(TP) / float(TP + FN) if float(TP + FN) != 0 else 0
-    # specificity（特异性）：正确预测为负类的样本数占所有实际负类样本数的比例。
     specificity = float(TN) / float(TN + FP) if float(TN + FP) != 0 else 0
-    # f1_or_dsc（F1分数或Dice系数）：综合考虑预测的精确性和召回率，是模型性能的一个指标。
     f1_or_dsc = float(2 * TP) / float(2 * TP + FP + FN) if float(2 * TP + FP + FN) != 0 else 0
-    # miou（平均交并比）：是模型预测的正类和实际正类之间的交并比的平均值。
     miou = float(TP) / float(TP + FP + FN) if float(TP + FP + FN) != 0 else 0
 
     log_info = f'{datetime.now()}  Test epoch: {epoch}, loss: {mean_loss:.4f}, miou: {miou:4f}, f1_or_dsc: {f1_or_dsc:4f}, accuracy: {accuracy:4f}, \
@@ -97,8 +88,7 @@ def test(test_loader, model, optimizer, epoch):
     return mean_loss
 
 if __name__ == '__main__':
-    #set_seed(42)  # Set the random seed for reproducibility
-    model_name = 'MDI_Net'
+    model_name = 'MGFI_Net'
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--testsize', type=int,
@@ -120,20 +110,20 @@ if __name__ == '__main__':
                         default=False, help='choose to do random flip rotation')
 
     parser.add_argument('--save_model', type=str,
-                        default='/home/ta/Project/SwinPA-Sparse/result' + model_name + '/')
+                        default='/home/ta/Project/MDI_Net/result' + model_name + '/')
 
     parser.add_argument('--pth_path', type=str,
-                        default='/home/ta/Project/MDI-Net-main/result/ISIC2018/MDI_Net/rank_1.pth')
+                        default='/home/ta/Project/MDI_Net/result/ISIC2018/MDI_Net/rank_1.pth')
 
     opt = parser.parse_args()
 
-    logging.basicConfig(filename='/home/ta/Project/MDI-Net-main/test/log/ISIC2018/MDI_Net.log',
+    logging.basicConfig(filename='/home/ta/Project/MDI_Net/test/log/ISIC2018/MDI_Net.log',
                         format='[%(asctime)s-%(filename)s-%(levelname)s:%(message)s]',
                         level=logging.INFO, filemode='a', datefmt='%Y-%m-%d %I:%M:%S %p')
     logging.info('opt:{}'.format(opt))
 
     torch.cuda.set_device(opt.gpu_ids)  # set your gpu device
-    model = MDI_Net().cuda()
+    model = MGFI_Net().cuda()
 
     params = model.parameters()
 
